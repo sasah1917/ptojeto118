23c1b1a79204f7400275516993af8e03e79f621b
sasah1917 <92746412+sasah1917@users.noreply.github.com>
2023-02-15 15:44:33 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, jsonify
+from model_prediction import *
+
+app = Flask(__name__)
+
+text=""
+predicted_emotion=""
+predicted_emotion_img_url=""
+
+@app.route("/")
+def home():
+    entries = show_entry()
+    return render_template("index.html", entries=entries)
+    
+
+@app.route("/predict-emotion", methods=["POST"])
+def predict_emotion():
+    input_text = request.json.get("text")
+    if not input_text:
+        return jsonify({
+            "status": "error",
+            "message": "Digite um texto para prever a emoção!"
+        }), 400
+    else:
+        predicted_emotion, predicted_emotion_img_url = predict(input_text)                         
+        return jsonify({
+            "data": {
+                "predicted_emotion": predicted_emotion,
+                "predicted_emotion_img_url": predicted_emotion_img_url
+            },
+            "status": "success"
+        }), 200
+        
+#Escreva o código da API aqui
+@app.route('/save-entry', methods = ['POST'])
+def save_entry():
+    #obter a data , a emocao e texto
+    date = request.json.get("date")
+    emotion = request.json.get("emotion")
+    save_text = request.json.get("text")
+    #entrada csv
+    save_text = save_text.replace('\n', "")
+    entry = f'{date},{save_text},{emotion}\n'
+    with open("./static/assets/data_files/data_entry.csv", "a") as f:
+        f.write(entry)
+    return jsonify('Success')
+                
+if __name__ == "__main__":
+    app.run(debug=True)
+
